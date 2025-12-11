e2e37049275de0ff45b1f7041001676f8c327179
o0travaa <o0travaa@gmail.com>
2025-12-11 15:18:58 +0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+if ! command -v yay >/dev/null 2>&1; then
+  echo "Error. yay not found."
+  exit 1
+fi
+
+echo "Installing paсkages..."
+sudo pacman -S --needed hyprland kitty waybar ttf-jetbrains-mono
+yay -S --needed ttf-jetbrains-mono-nerd catppuccin-theme-gtk-mocha
+
+echo "Copying..."
+cp -r ./config/hypr   ~/.config/hypr
+cp -r ./config/waybar ~/.config/waybar
+cp -r ./config/kitty  ~/.config/kitty
+cp -r ./config/gtk-3.0 ~/.config/gtk-3.0
+cp -r ./config/gtk-4.0 ~/.config/gtk-4.0
