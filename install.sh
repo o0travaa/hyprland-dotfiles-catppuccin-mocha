232fd885b6caba227f9c9221f84b9a296ba81ab5
o0trava <o0travaa@gmail.com>
2025-12-11 15:50:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if ! command -v yay >/dev/null 2>&1; then
 fi
 
 echo "Installing paсkages..."
-sudo pacman -S --needed hyprland kitty waybar ttf-jetbrains-mono
+sudo pacman -S --needed hyprland kitty waybar ttf-jetbrains-mono wofi
 yay -S --needed ttf-jetbrains-mono-nerd catppuccin-theme-gtk-mocha
 
 echo "Copying..."
